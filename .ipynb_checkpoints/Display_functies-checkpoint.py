6d10a0f3c38cde03f992c95756ae6730b53842b0
casperbkker <casperbkkr@gmail.com>
2019-06-18 15:04:13 +0200

    Klikbaar gemaakt

--- .ipynb_checkpoints/Display_functies-checkpoint.py
+++ .ipynb_checkpoints/Display_functies-checkpoint.py
@@ -3,17 +3,38 @@ import Dek_functies as deks
 import random
 import pygame
 
+def locatie_kaarten(X,Y):
+    Y_coordinaten = []
+    hoogte = (Y-100)/4
+    X_coordinaten = []
+    breedte = (X-100)/3
+    for i in range(3):
+        X_coordinaten.append(50+int(i*breedte))
+    for j in range(4):
+        Y_coordinaten.append(50+int(j*hoogte))
+    return Y_coordinaten, X_coordinaten #lijst met coördinaten waar kaarten moeten worden geplaatst.
 
 
-def card_locations(display_size_x, display_size_y):
+def tussen(positie_muis, hoekpunten, x_of_y):
+    for i in range(len(hoekpunten)):
+        if hoekpunten[i]<=positie_muis<=hoekpunten[i]+x_of_y:
+            return i
+    return 42
+
+def waar_geklikt(positie_muis, plekx, pleky):
     
-    return locations #lijst met coördinaten waar kaarten moeten worden geplaatst.
-
-
-def show_cards(gedeelde_kaarten, display_size):
+    locatie_dict= {"00":0,"10":1,"20":2,
+                   "01":3,"11":4,"21":5,
+                   "02":6,"12":7,"22":8,
+                   "03":9,"13":10,"23":11,}
     
-print("hallo")
-        
+    x = str(tussen(positie_muis[0], plekx, 200))
+    y = str(tussen(positie_muis[1], pleky, 100))
+    if x+y in locatie_dict:
+        geklikte_kaart = locatie_dict[x+y]
+        return geklikte_kaart
+    else:
+        return "zwart"
         
         
         
