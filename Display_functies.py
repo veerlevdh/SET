35b4cc65449ec440480717570b773f903e98b219
casperbkker <casperbkkr@gmail.com>
2019-06-21 21:48:53 +0200

    no message

--- Display_functies.py
+++ Display_functies.py
@@ -12,36 +12,31 @@ def locatie_kaarten(X,Y):
         X_coordinaten.append(50+int(i*breedte))
     for j in range(4):
         Y_coordinaten.append(50+int(j*hoogte))
-    return Y_coordinaten, X_coordinaten #lijst met coördinaten waar kaarten moeten worden geplaatst.
+    return [X_coordinaten, Y_coordinaten] #lijst met coördinaten waar kaarten moeten worden geplaatst.
 
 
 def tussen(positie_muis, hoekpunten, x_of_y):
     for i in range(len(hoekpunten)):
-        if hoekpunten[i]<=positie_muis<=hoekpunten[i]+x_of_y:
+        if hoekpunten[i] <= positie_muis <= hoekpunten[i]+x_of_y:
             return i
     return 42
 
-def waar_geklikt(positie_muis, plekx, pleky):
+def waar_geklikt(positie_muis, plek_x_y):
     
     locatie_dict= {"00":0,"10":1,"20":2,
                    "01":3,"11":4,"21":5,
                    "02":6,"12":7,"22":8,
                    "03":9,"13":10,"23":11,}
     
-    x = str(tussen(positie_muis[0], plekx, 200))
-    y = str(tussen(positie_muis[1], pleky, 100))
-    if x+y in locatie_dict:
+    x = str(tussen(positie_muis[0], plek_x_y[0], 200))
+    y = str(tussen(positie_muis[1], plek_x_y[1], 100))
+    if x + y in locatie_dict:
         geklikte_kaart = locatie_dict[x+y]
         return geklikte_kaart
     else:
         return "zwart"
 
-def print_scherm(scherm):
-    nummer = 0
-    for y in range(0,4):
-        for x in range(0,3):
-            scherm.blit(gedeelde_kaarten[nummer].image, (plekx[x], pleky[y]))
-            nummer+=1
+
 
 
         
--- Run.py
+++ Run.py
@@ -5,25 +5,11 @@ import Kaart_class as cards
 import Display_functies as disp
 import time, sys, random, pygame
 
-#setup voor het spel
-alle_kaarten = dek.maak_dek()
-gedeelde_kaarten = dek.eerste_keer_delen(alle_kaarten)
-gevonden_sets = dek.alle_sets_vinden(gedeelde_kaarten)
-zwart = (0,0,0) 
-rood=(255,0,0)  
-# bepaal de breedte (X) en hoogte (Y) van je scherm 
-X = 1000
-Y = 600
-pleky, plekx = disp.locatie_kaarten(X,Y)
-
-gekozen_kaarten = []
-punten = 0
-
 def print_scherm(scherm):
     nummer = 0
     for y in range(0,4):
         for x in range(0,3):
-            scherm.blit(gedeelde_kaarten[nummer].image, (plekx[x], pleky[y]))
+            scherm.blit(gedeelde_kaarten[nummer].image, (plek_x_y[0][x], plek_x_y[1][y]))
             nummer+=1
 
 def tijd_over(start_tijd):
@@ -32,6 +18,21 @@ def tijd_over(start_tijd):
             return True, tijd
         else:
             return False, tijd
+
+#setup voor het spel
+alle_kaarten = dek.maak_dek()
+gedeelde_kaarten = dek.eerste_keer_delen(alle_kaarten)
+gevonden_sets = dek.alle_sets_vinden(gedeelde_kaarten)
+zwart = (0,0,0) 
+rood = (255,0,0)  
+
+# bepaal de breedte (X) en hoogte (Y) van je scherm 
+X = 1000
+Y = 600
+plek_x_y = disp.locatie_kaarten(X,Y)
+
+gekozen_kaarten = []
+punten = 0
 
 pygame.init() 
 # maak een scherm van grootte X bij Y 
@@ -46,39 +47,39 @@ Punten_telling = myFont.render('Aantal punten: ' + str(punten), 1, (255,0,0))
 start_tijd = int(time.time())
 
 while True : 
-    
+    pygame.display.flip()
 # vul je scherm zwart
     display_surface.fill(zwart) 
 
 # plak de afbeeldingen van de twaalf kaarten op de locaties die we eerder met de functie locatie_kaarten hebben bepaald
     print_scherm(display_surface)
     
-#puntentelling    
+#puntentelling  
+    Punten_telling = myFont.render('Aantal punten: ' + str(punten), 1, (255,0,0))
     display_surface.blit(Punten_telling,(50,15))
     
 #Tijd bijhouden
-
-    Punten_telling = myFont.render('Aantal punten: ' + str(punten), 1, (255,0,0))
     if tijd_over(start_tijd)[0] is True:
         text = myFont.render(("Tijd: " + str(tijd_over(start_tijd)[1])  +" seconden"), 1, (255,0,0))
-        display_surface.blit(text, (plekx[2],15))
+        display_surface.blit(text, (plek_x_y[0][2],15))
     else:
         text = myFont.render("Te laat!", 1, (255,0,0))
-        display_surface.blit(text, (plekx[2], 15))
+        display_surface.blit(text, (plek_x_y[0][2], 15))
         dek.set_vervangen(gedeelde_kaarten, alle_kaarten)
         gevonden_sets = dek.alle_sets_vinden(alle_kaarten)
         gekozen_kaarten = []
 
         start_tijd = int(time.time())
         punten -= 1
-    pygame.display.flip()
+    
 
     # Haal alle events op
     for event in pygame.event.get() : 
+        #Haal (x,y) van muis op  
         positie_muis = pygame.mouse.get_pos() 
-        if event.type == pygame.MOUSEBUTTONDOWN: #Als er op muis is geklikt
-            #haal (x,y) van muis op   
-            klik_locatie = disp.waar_geklikt(positie_muis, plekx, pleky)
+        
+        if event.type == pygame.MOUSEBUTTONDOWN: #Als er op muis is geklikt 
+            klik_locatie = disp.waar_geklikt(positie_muis, plek_x_y)
             if klik_locatie is not "zwart": #als op een kaart geklikt
                 if klik_locatie not in gekozen_kaarten:
                     gekozen_kaarten.append(klik_locatie)  
@@ -107,7 +108,6 @@ while True :
         # then quitting the pygame 
         # and program both. 
         if event.type == pygame.QUIT : 
-
             pygame.quit() 
   
             quit() 
